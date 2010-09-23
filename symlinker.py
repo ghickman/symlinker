e1cb8fdfd81aece15044dc0ecdc049b9e28910ba
George Hickman <george@ghickman.co.uk>
2010-09-23 09:33:05 +0100

    Added command line parser for ease of use

--- symlinker.py
+++ symlinker.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 
 __author__ = 'George Hickman'
-__version__ = '0.1'
+__version__ = '0.2'
 
+from optparse import OptionParser
 import os
 
-start = '/path/to/start'
-finish = '/path/to/finish'
+parser = OptionParser(usage="symlinker.py <start> <finish>",
+                        version="Symlinker %s" % __version__)
+options, args = parser.parse_args()
+
+try:
+    start = args[0]
+    finish = args[1]
+except IndexError:
+    parser.error('Either a start or finish directory was not given')
 
 for directory in os.listdir(start):
     origin = os.path.join(start, directory)+'/'
